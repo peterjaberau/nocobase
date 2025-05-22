3d57bae90ef41b46d1531617c63c08f28ec188cc
peterjaberau <peterjaberau@gmail.com>
2025-05-22 19:55:05 +1000

    initial commit

--- packages/core/client/src/icon/demos/antd-icon.tsx
+++
@@ -1,6 +0,0 @@
-
-
-import { Icon } from '@easyflow/client';
-import React from 'react';
-
-export default () => <Icon type="BookOutlined" />;
--- packages/core/client/src/schema-component/antd/pagination/demos/basic.tsx
+++
@@ -1,8 +0,0 @@
-
-
-import { Pagination } from '@easyflow/client';
-import React from 'react';
-
-const App: React.FC = () => <Pagination defaultCurrent={1} total={50} />;
-
-export default App;
